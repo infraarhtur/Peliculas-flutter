416fdcf251e5b688e1857fa20223d64c8ffbc056
Arhtur monsalve <luismonsalve@cbit-online.com>
2020-02-14 18:03:46 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas2/src/pages/home_page.dart';
+import 'package:peliculas2/src/pages/peliculas_detalle.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Peliculas',
+      initialRoute: '/',
+      routes: {
+        '/':(BuildContext context) => HomePage(),
+        'detalle':(BuildContext context) => PeliculaDetalle()
+      },
+   
+    );
+  }
+}
